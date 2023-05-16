53bf3450014bc26718192f3c0c5eba5b70226870
NicoDayo <106494279+NicoDayo@users.noreply.github.com>
2023-05-16 09:53:50 -0700

    Create_db unit test

--- create_db.py
+++ create_db.py
@@ -5,7 +5,11 @@ def database():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(os.path.join(current_dir, 'notes.db'))
     c = conn.cursor()
-    c.execute('''CREATE TABLE notes
-                    (id INTEGER PRIMARY KEY, title TEXT, body TEXT, updated TEXT)''')
-    conn.commit()
+    c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='notes'")
+    result = c.fetchone()
+
+    if result is None:
+        c.execute('''CREATE TABLE notes
+                        (id INTEGER PRIMARY KEY, title TEXT, body TEXT, updated TEXT)''')
+        conn.commit()
     conn.close()
