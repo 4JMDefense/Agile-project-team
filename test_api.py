bd20e7fa4cdcf1ea01633cd59c44ed3b5c1ca8ef
NicoDayo <106494279+NicoDayo@users.noreply.github.com>
2023-05-23 15:37:55 -0700

    comments and adjustments

--- test_api.py
+++ test_api.py
@@ -3,7 +3,7 @@ from flask_testing import TestCase
 from api import app
 
 class RoutesTest(TestCase):
-    def create_app(self):
+    def create_app(self): # instance of flask application made
         app.config['TESTING'] = True
         return app
 
@@ -11,7 +11,7 @@ class RoutesTest(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def invalid_route(self): # tests for incorrect routes, should send a 404 response
+    def test_invalid_route(self): # tests for incorrect routes, should send a 404 response
         response = self.client.get('/wrong_route')
         self.assertEqual(response.status_code, 404)
 
